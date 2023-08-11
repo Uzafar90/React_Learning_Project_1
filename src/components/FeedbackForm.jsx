988edafa3822d70bc193f333c6f192eab5bed8dd
“uzafar90” <utbazafar@yahoo.com>
2023-08-10 20:56:47 -0400

    file update

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,10 +4,24 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
 	const [text, setText] = useState('');
+	const [rating, setRating] = useState(10);
+	const [btnDisabled, setBtnDisabled] = useState(true);
+	const [message, setMessage] = useState('');
 
 	const handleTextChange = (e) => {
+		if (text === '') {
+			setBtnDisabled(true);
+			setMessage(null);
+		} else if (text !== '' && text.trim().length <= 10) {
+			setMessage('Text must be at least 10 characters long');
+			setBtnDisabled(true);
+		} else {
+			setMessage(null);
+			setBtnDisabled(false);
+		}
 		setText(e.target.value);
 	};
+	console.log(handleTextChange);
 
 	return (
 		<Card>
@@ -20,8 +34,11 @@ function FeedbackForm() {
 						placeholder="Write a review"
 						value={text}
 					/>
-					<Button type="submit">Send</Button>
+					<Button type="submit" isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+				{message && <div className="message">{message}</div>}
 			</form>
 		</Card>
 	);
